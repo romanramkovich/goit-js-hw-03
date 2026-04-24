98b37fb100942eb734fa8f25d5fb121263e14059
Roman Ramkovich <romanramkovich@gmail.com>
2026-04-24 08:38:09 +0300

    Objects

---
+++ js/task-1.js
@@ -0,0 +1,27 @@
+// function isEnoughCapacity(products, containerSize) {
+
+// let totalCounts = 0;
+// const counts = Object.values(products)
+// for (const count of counts) {
+// totalCounts += count
+// }
+// // console.log(totalCounts)
+
+// return totalCounts <= containerSize
+// }
+
+// console.log(
+//   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+// );
+// console.log(
+//   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+// ); // false
+
+// console.log(
+//   isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+// ); // true
+
+// console.log(
+//   isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+// ); // false
+
